515267393a3d506382fd994dfb0f2322bb080037
duynt <trungduy150388@gmail.com>
2012-09-01 15:17:01 +0700

    commit TodoList with ListView Day3

---
+++ duynt/Day3/TodoList/src/com/techmaster/todolist/Constant.java
@@ -0,0 +1,13 @@
+package com.techmaster.todolist;
+
+public class Constant {
+	public static String NOTE_DESCRIBLE = "DESCRIBLE";
+	public static String NOTE_CONTENT = "CONTENT";
+	public static String NOTE_DATE = "DATE";
+	public static String NOTE_NAME = "NAME";
+	public static String NOTE_CHANGE = "CHANGE";
+	public static final int REQUEST_ADD = 1;
+	public static final int REQUEST_EDIT = 4;
+	public static final int SAVE = 2;
+	public static final int CANCEL = 3;
+}
